334d858bc00ab625c45eec61b09e72031f746b86
Toapanta Carlos <carlosalex917@gmail.com>
2022-11-30 23:49:55 -0500

    Ofertas de Trabajo con style y html

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -3,8 +3,98 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"></script>
     <link rel='stylesheet' id='sage/main.css-css'  href='<?php echo base_url();?>/css/main.css' media='all' />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500&display=swap" rel="stylesheet">
+    <style>
+        .contenedor,
+.wave-contenedor{
+    width: 90%;
+    max-width: 1000px;
+    overflow: hidden;
+    margin: auto;
+    padding: 0 0 80px 0;
+}
+.contenedor{
+    padding: 80px 0;
+}
+.titulo{
+    font-weight: 300;
+    font-size: 35px;
+    text-align: center;
+    margin-bottom: 30px;
+}
+.titulo.left{
+    text-align: left;
+}
+
+.titulo.right{
+    text-align: right;
+}
+
+.content-cards{
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+}
+.card{
+    width: 30%;
+    text-align: center;
+    height: 300px;
+    box-shadow: 0 0 3px 0 rgba(0, 0, 0, .5);
+    transform: scale(1);
+    transition: transform 0.6s;
+}
+.card:hover{
+    box-shadow: 0 0 6px 0 rgba(0, 0, 0, .5);
+    transform: scale(1.03);
+    cursor: pointer;
+}
+
+.card i{
+    margin: 30px 0 20px 0;
+    color: #cc9199;
+    font-size: 50px;
+}
+
+.card p{
+    font-weight: 300;
+    font-size: 25px;
+    margin-bottom: 10px;
+}
+.card a{
+    display: inline-block;
+    text-decoration: none;
+    padding: 12px 15px;
+    background: #a18cd1;
+    border-radius: 8px;
+    color: #fff;
+}
+    </style>
 
 <barter id="bootstrap_PiePagina" class="content-info custom_PiePagina bootstrap_PiePagina principal_PiePagina1">
+<section class="cards contenedor">
+        <h2 class="titulo">Ofertas de Trabajo</h2>
+        <div class="content-cards">
+            <article class="card">
+                <i class="far fa-clone"></i>
+                <h3>Estilizados</h3>
+                <p>Tratamientos para el pelaje</p>
+                <a href="" class="cta">Estilizacion</a>
+            </article>
+            <article class="card">
+                <i class="fas fa-database"></i>
+                <h3>Manicure</h3>
+                <p>Cuidamos las patas de tu animal </p>
+                <a href="" class="cta">Manicure</a>
+            </article>
+            <article class="card">
+                <i class="far fa-object-group"></i>
+                <h3>Alimentacion</h3>
+                <p>Plan de alimentacion animal</p>
+                <a href="" class="cta">Alimentacion</a>
+            </article>
+        </div>
+    </section>
 
 <div class="section-row">
 
@@ -134,6 +224,7 @@ Todo está 100% en español.
     </div>
   
 </div>
+
 
 </barter>
 </body>
